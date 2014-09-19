f3a7a7e2a01a9dde93a33590d7932807cc476556
Salim Kapadia <salimk786@gmail.com>
2014-09-19 02:53:22 -0700

    adding seed data for the db

---
+++ test/database/migrations/20140919093512-seed-users.js
@@ -0,0 +1,14 @@
+var async = require('async');
+
+exports.up = function (db, callback) {
+    async.series([
+        db.runSql.bind(db,
+        	'INSERT INTO user (`username`, `email`, `password`, `salt`, `group_id`, `status`, `created`, `modified`) VALUES ("testjohndoe","testemail@test.com","testpassword","somesaltkey",10,"active",NOW(),NOW())', []),
+    ], callback);
+};
+
+exports.down = function (db, callback) {
+    async.series([
+        db.runSql.bind(db,'DELETE FROM user where email = "testemail@test.com"', []),        
+    ], callback);
+};
