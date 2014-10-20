12e0c42d33d93277f55fdd1a46b8fac10acbf348
Salim Kapadia <salimk786@gmail.com>
2014-10-19 17:38:47 -0700

    moved optional hapi plugins to config file

--- index.js
+++ index.js
@@ -6,31 +6,27 @@ var config      = require('./config/config');
 var routes      = require('./routes');
 
 var server = new Hapi.Server(Number(config.hapi.port));
-	server.pack.register([
-		{plugin:require('lout')}, // Self documenting tool for routes with http method.
-		{plugin:require('bell')}, // Social login plugin
-		{plugin:require('hapi-auth-cookie')} // Session management plugin
-		], function(err) {
-		    if (err) throw err;
+server.pack.register(config.hapi.plugins, function(err) {
+	    if (err) throw err;
 
-		    // Configure the plugins that are now attached to the server.
-		    for (var index=0; index < config.plugin.auth.strategy.length; index++){
-				server.auth.strategy(
-					config.plugin.auth.strategy[index].name,
-					config.plugin.auth.strategy[index].scheme,
-					config.plugin.auth.strategy[index].options
-				);
-		    }
+	    // Configure the plugins that are now attached to the server.
+	    for (var index=0; index < config.hapi.auth.strategy.length; index++){
+			server.auth.strategy(
+				config.hapi.auth.strategy[index].name,
+				config.hapi.auth.strategy[index].scheme,
+				config.hapi.auth.strategy[index].options
+			);
+	    }
 
-		    // Configure database connection
-			var dbHandler = require('./database');
-			dbHandler.init(config.database);
+	    // Configure database connection
+		var dbHandler = require('./database');
+		dbHandler.init(config.database);
 
-		    // Add configured routes to the server routes
-			for (var tmp in routes){
-			    server.route(routes[tmp]);
-			};
-	});
+	    // Add configured routes to the server routes
+		for (var tmp in routes){
+		    server.route(routes[tmp]);
+		};
+});
 
 // If the script is being required as a module by another script, don’t start the server.
 if (!module.parent)
