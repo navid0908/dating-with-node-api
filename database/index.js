413f81709aabb77f81f2cb2b92dffb911aa865dc
Salim Kapadia <salimk786@gmail.com>
2014-11-12 09:00:42 -0800

    removing unused reference and adding comments

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ models = {
 		var self = this;
 		var knex = require('knex')(options); //pass in db config options.
 		var Bookshelf = require('bookshelf');
-		var datingWithNode = Bookshelf.dbh = Bookshelf(knex);
+		Bookshelf.dbh = Bookshelf(knex);
 
 
         // One off inclusion of Base file.
--- database/models/base.js
+++ database/models/base.js
@@ -4,7 +4,11 @@ var baseModel = require('bookshelf').dbh;
 // Load the registry plugin, which helps us avoid circular dependencies
 baseModel.plugin('registry');
 
-baseModel.Model = baseModel.Model.extend({},{});
+baseModel.Model = baseModel.Model.extend({
+	//object properties
+},{
+	//static properties
+});
 
 // Export baseModel for use elsewhere
 module.exports = baseModel;
