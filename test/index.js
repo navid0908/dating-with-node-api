8241cf73229c47661fd54daad0ac89666df79e5b
Salim Kapadia <salimk786@gmail.com>
2015-02-26 16:21:49 -0600

    removing test/index file as the lab framework autoloads everything within test

--- test/index.js
+++
@@ -1,4 +0,0 @@
-// Load all files in this directory -  i.e. build a hash/tree of the exports from the ./ directory
-var requireDirectory = require('require-directory');
-module.exports = requireDirectory(module);
-
